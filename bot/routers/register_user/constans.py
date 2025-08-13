e2d5afdc80393a9b86bbb943d709df0d5f838ad0
Levlink773 <cleandev9@gmail.com>
2025-08-14 02:36:42 +0300

    comi 14.2

--- bot/routers/register_user/constans.py
+++ bot/routers/register_user/constans.py
@@ -2,8 +2,8 @@ from aiogram.types import FSInputFile
 
 
 START_REGISTER_PHOTO = FSInputFile("src/register_user/pre_register.png")
-CREATER_CHARACTER_PHOTO = FSInputFile("src/register_user/create_character.jpg") 
-SEND_NAME_CHARACTER_PHOTO = FSInputFile("src/register_user/send_name.jpg")
+CREATER_CHARACTER_PHOTO = FSInputFile("src/register_user/create_character.png")
+SEND_NAME_CHARACTER_PHOTO = FSInputFile("src/register_user/send_name.png")
 SELECT_POSITION_PHOTO = FSInputFile("src/register_user/select_position.jpg") 
 TERRITORY_ACADEMY_PHOTO = FSInputFile("src/register_user/territory_academy.jpg") 
 JOIN_TO_CLUB_PHOTO = FSInputFile("src/register_user/join_to_club.jpg") 
